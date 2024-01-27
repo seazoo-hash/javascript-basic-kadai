3e8c30df61576540b719e710dbf09d93922b2482
seazoo-hash <155947973+seazoo-hash@users.noreply.github.com>
2024-01-27 23:47:47 +0900

    課題２０のフォルダをアップロード

---
+++ kadai_020/event.js
@@ -0,0 +1,10 @@
+const chengebtn = document.getElementById('btn');
+ 
+ // HTML要素がクリックされたときにイベント処理を実行する
+ chengebtn.addEventListener('click', () => {
+   // すべてのチェックボックスを配列風のデータで取得する
+   const addText = document.getElementById('text');
+
+   addText.textContent = 'ボタンをクリックしました';
+ 
+ });
